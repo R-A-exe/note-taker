b16db74f9daf21616fc3cdc377aad8811f495da8
R-A-exe <royatalla.ra@gmail.com>
2021-01-31 18:05:28 -0500

    Bug fix for static

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { nextTick } = require('process');
 
 const app = express();
 var PORT = process.env.PORT || 3000;
@@ -7,23 +9,17 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'notes.html'));
-});
-
-app.get('/*', (req, res) => {
+app.get('/*', (req, res, next) => {
     if (!req.url.includes("/api")) {
-        res.sendFile(path.join(__dirname, '/public', req.url.substr(1) + '.html'), function (err) {
-            res.status(404);
-            res.send("File not found")
+        res.sendFile(path.join(__dirname, '/public/', req.url.substr(1) + '.html'), err => {
+            if(err) res.status(404).send("File not found");
         });
+    } else {
+        next();
     }
 });
 
 
-
-
 app.listen(PORT, () => {
     console.log(`Now listening to port ${PORT}`);
-})
-
+});
