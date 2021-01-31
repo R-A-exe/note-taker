e3b991bc857c7662c79852b1c6982523b99f58ce
R-A-exe <royatalla.ra@gmail.com>
2021-01-31 16:50:04 -0500

    completed static response

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 var PORT = process.env.PORT || 3000;
@@ -7,17 +7,23 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-// app.get('/notes', (req, res)=>{
-//     res.sendFile(path.join(public,'notes.html'));
-// });
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'notes.html'));
+});
 
-app.get('/*', (req, res)=>{
-    // res.render(path.join(__dirname, 'public/', 'index.html'));
-    // res.sendFile(path.join(__dirname, 'public/'));
-});1
+app.get('/*', (req, res) => {
+    if (!req.url.includes("/api")) {
+        res.sendFile(path.join(__dirname, '/public', req.url.substr(1) + '.html'), function (err) {
+            res.status(404);
+            res.send("File not found")
+        });
+    }
+});
 
 
-app.listen(PORT, ()=>{
+
+
+app.listen(PORT, () => {
     console.log(`Now listening to port ${PORT}`);
 })
 
